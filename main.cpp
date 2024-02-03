f514b6d057f26bc263cbcbbc148a81a39ea094f6
Shaunakm07 <137065822+Shaunakm07@users.noreply.github.com>
2024-02-03 22:41:23 +0000

    http://www.usaco.org/index.php?page=viewproblem2&cpid=891

--- main.cpp
+++ main.cpp
@@ -2,12 +2,58 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include "problem_1/problem_1.h"
+
 
 using namespace std;
 
-int main()
-{
-    problem_1();
+int main() {
+    freopen("shell.in", "r", stdin);
 
+    string changes;
+    cin >> changes;
+
+    int no_of_swaps;
+    no_of_swaps = stoi(changes);
+
+    vector<int> pebble_position(3);
+
+    for (int i = 0; i < no_of_swaps; i++)
+    {
+        pebble_position[i] = i;
+    }
+
+    vector<int> count(3);
+
+    for (int i = 0; i < no_of_swaps; i++)
+    {
+        int a;
+        int b;
+        int c;
+
+        cin >> a >> b >> c;
+
+        a--;
+        b-- ;
+        c--;
+
+        for (int j = 0; j < 3; j++)
+        {
+            int *instance = &pebble_position[j];
+            if (a == *instance)
+            {
+                *instance = b;
+            } else if (b == *instance) {
+                *instance = a;
+            }
+
+            if(c == *instance)
+            {
+                count[j] ++;
+            }
+        }
+
+
+    }
+
+    cout << std::max({count[0], count[1], count[2]});
 }
